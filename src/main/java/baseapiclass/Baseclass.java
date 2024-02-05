7928fed48d3fbdabc34a0d92672558ebf0054159
Niroosha H <152973095+NirooshaH@users.noreply.github.com>
2024-02-05 10:45:24 +0530

    Update Baseclass.java
    commiting the comments

--- src/main/java/baseapiclass/Baseclass.java
+++ src/main/java/baseapiclass/Baseclass.java
@@ -6,12 +6,28 @@ import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
 public class Baseclass {
+	//What are the different HTTP Methods we use in RestAPI
+	//Get //POST //PUT //PATCH //Delete //when ever i create my request 
+	
+	//Basically why did i create all this methods because//i want reduce my redundency(repetation)
+	
+	//this is a static method because i don't have to create a object for this.
+	//my get method will have a body?
+	//get method is used only to fetch the data from the server 
+	
 	public  static Response Getrequest(String requesturl) {
 		RequestSpecification requestspecification=RestAssured.given();
 		requestspecification.contentType(ContentType.JSON);
+		
+		//i am providing the get method along with the request URL	
+		//This get method is going to execute and fetch the resposne that response i am stroing it in a variable
+		
 		Response response=requestspecification.get(requesturl);
+		//This return response will be called in my test method //where ever i create a test method to execute i call the return 
 		return response;	
 	}
+	//post method is used to create the data Login, 
+	//This is the post request with body
 	public static Response Postrequest(String requesturl, String bearer_token, String requestpayload) {
 		RequestSpecification requestSpecification=RestAssured.given().body(requestpayload);
 		requestSpecification.contentType(ContentType.JSON);
@@ -19,6 +35,7 @@ public class Baseclass {
 		Response response=requestSpecification.post(requesturl);
 		return response;		
 	}
+	//This is the post request without body
 	public static Response putrequest(String requesturl,String bearer_token, String requestpayload) {
 		RequestSpecification requestSpecification=RestAssured.given().body(requestpayload);
 		requestSpecification.contentType(ContentType.JSON);
