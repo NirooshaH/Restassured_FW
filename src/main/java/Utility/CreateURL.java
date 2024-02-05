ef82518d3cd66e179c58040acb58764c9cd0582a
Niroosha H <152973095+NirooshaH@users.noreply.github.com>
2024-02-05 10:47:18 +0530

    Update CreateURL.java
    commiting

--- src/main/java/Utility/CreateURL.java
+++ src/main/java/Utility/CreateURL.java
@@ -9,7 +9,7 @@ public class CreateURL {
 		return baseuri;
 	}
 	
-
+ //don't you think my baseuri should also be concatenated with resource
 	public static String getbaseuri(String resource)
 	{
 		return baseuri + resource;  // baseuri concatination with resource
