8fe334da02bbda63a709a4c268a093d8edc454eb
Niroosha H <152973095+NirooshaH@users.noreply.github.com>
2024-02-05 10:47:49 +0530

    Update Authentication.java
    commiting

--- src/main/java/Utility/Authentication.java
+++ src/main/java/Utility/Authentication.java
@@ -5,7 +5,9 @@ public class Authentication {
 		public final static String bearer_token="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";
 				//"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"; 
 		
-		
+		//so that i don't have to create an object
+	//Authentication authen = new Authentication();
+	
 		public static String getbearertoken()
 				{
 					return bearer_token;			
